f171a0c7a35bf6d5a39f51563b6a8c70fd6195f8
soeatsu <soeatsu@gmail.com>
2022-08-01 00:31:51 +0900

    プロジェクト ファイルを追加します。

---
+++ PetData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ML.Data;
+
+namespace Sample_MLNET
+{
+    /// <summary>
+    /// 学習データカラムの定義
+    /// </summary>
+    public class PetData
+    {
+        public string? Breed { get; set; }
+        public string? ImageFilePath {  get; set; }
+    }
+
+    public class PetDataPrediction : PetData
+    {
+        public string? PredictedBreed { get; set; }
+        public float[]? Score { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Sample_MLNET;
+
+//.txtパス
+string dataSetFilePath = @"C:.\DataSet\PetImages\_petImageFileList.txt";
+
+//パス一覧の読み込む
+var imagefilePaths = File.ReadAllLines(dataSetFilePath, System.Text.Encoding.Default);
+
+//データセットの作成
+var petDataSet = imagefilePaths.Select(path =>
+    new PetData()
+    {
+        //ファイル名に品種名が含まれているので、品種名を切り出してBreedに設定
+        Breed = path.Substring(path.LastIndexOf('\\') + 1,
+        path.LastIndexOf('_') - path.LastIndexOf('\\') - 1),
+        ImageFilePath = path
+    });
